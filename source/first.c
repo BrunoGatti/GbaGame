2cf7d4f2e1aca4d5a14c9f9f643055f333e3f388
Bruno <50294421+BrunoGatti@users.noreply.github.com>
2023-03-30 10:25:19 +0200

    ok
    ok

--- source/first.c
+++ source/first.c
@@ -1,16 +1,36 @@
-// First demo. You are not expected to understand it 
-// (don't spend too much time trying and read on).
-// But if you do understand (as a newbie): wow!
+#include <tonc.h>
 
 int main()
 {
-    *(unsigned int*)0x04000000 = 0x0403;
+    // Load the sprite image into memory
+    u32* spriteData = (u32*)malloc(32 * 32 / 2);
+    loadBmp("sprite.bmp", spriteData);
 
-    ((unsigned short*)0x06000000)[120+80*240] = 0x001F;
-    ((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
-    ((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
+    // Set the sprite's attributes
+    OBJATTR spriteAttr = {
+        ATTR0_SQUARE,           // Square shape
+        ATTR1_SIZE_32,          // 32x32 size
+        ATTR2_PALBANK(0) | 0,   // Use palette 0 and tile index 0
+    };
 
-    while(1);
+    // Copy the sprite data to OAM
+    oam_memset(&oam_main, 0, sizeof(oam_main));  // Clear OAM
+    oam_set(&oam_main, 0, 0, 0, 0, &spriteAttr); // Set sprite attributes
+
+    // Enable the display of sprites
+    REG_DISPCNT |= DCNT_OBJ | DCNT_OBJ_1D;
+
+    // Loop forever
+    while(1) {
+        // Wait for VBlank
+        vid_vsync();
+
+        // Update OAM
+        oam_copy(&oam_main, obj_buffer, 1);
+
+        // Update the screen
+        vid_flip();
+    }
 
     return 0;
 }
